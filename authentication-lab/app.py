50aff7670033d57ba5b59b1fc46fa962667e6310
ellaha23-meet <109908161+ellaha23-meet@users.noreply.github.com>
2022-08-01 13:00:36 +0300

    I finished
    was a nice lab!

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -10,11 +10,12 @@ config= {
   "messagingSenderId": "965372458361",
   "appId": "1:965372458361:web:2040960fd0e8f69c6e6ba8",
   "measurementId": "G-CH06891GD0",
-  "databaseURL":""
+  "databaseURL":"https://ella-s-firsts-firebase-project-default-rtdb.firebaseio.com/"
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
  
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -42,20 +43,33 @@ def signin():
 def signup():
    error = ""
    if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
+        email = request.form['email']
+        password = request.form['password']
+        try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            user= {"email": request.form["email"], "password": request.form["password"], "fullname": request.form["fullname"], "username": request.form["username"],"bio": request.form["bio"]}
+            db.child("Users").child(login_session['user']['localId']).set(user)
             return redirect(url_for('add_tweet'))
-       except:
-           error = "Authentication failed"
+        except:
+            error = "Authentication failed"
    return render_template("signup.html")
 
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method == 'POST':
+        try:
+           article = {'title': request.form['title'],'text': request.form['text'], "uid": login_session["user"] [localId]}
+           db.child("add_tweet").push(article)
+        except:
+           print("Couldn't add article")
     return render_template("add_tweet.html")
+
+@app.route('/all_tweets', methods=['GET', 'POST'])
+def all_tweets():
+    article = db.child("Users").child(login_session['user']['localId']).get().val()
+    return render_template("add_tweet.html", title=article["title"], text=article["text"])
 
 @app.route('/signout')
 def signout():
